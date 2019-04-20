9ca444837f3e2ded6381dd8d77ca393819fbdb08
Tomek <lektor1993@wp.pl>
2019-04-20 17:45:59 +0200

    pinned event handler in searchbar module

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../style.css';
 
 class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (<div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Image searching</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={this.onInputChange}></input>
                 </div>
 
             </form>
