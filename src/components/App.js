59e2637321116048a3efce64355e71a23ec5ae2e
Tomek <lektor1993@wp.pl>
2019-04-20 22:34:43 +0200

    implemented axios request to api

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import '../style.css';
 
-class App {
-    onSearchSubmit(term) {
-        console.log(term);
 
+class App extends React.Component {
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID 68d032e13466bfda6678062087932615ddd346fcd57f8d44bdb9b253b4de358d'
+            }
+        });
     }
 
     render() {
         return (
             <div className="ui container">
-                <SearchBar onSearchSubmit={this.onSearchSubmit} />
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (event) => {
         event.preventDefault();
         this.props.onSubmit(this.state.term);
-    }
+    };
     render() {
         return (
             <div className="ui segment">
